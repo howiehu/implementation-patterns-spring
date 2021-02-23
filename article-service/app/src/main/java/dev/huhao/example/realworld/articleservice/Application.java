338eaf46b59e33fa19983f289cab54f69ecc2d09
Hao Hu <huhao@outlook.com>
2021-02-23 17:08:16 +0800

    build: initialize the layerd architecture

--- article-service/app/src/main/java/dev/huhao/example/realworld/articleservice/Application.java
+++ article-service/app/src/main/java/dev/huhao/example/realworld/articleservice/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
