eefe5b65def3379e6ad3bcfdfbdb80d455a1a735
Hao Hu <huhao@outlook.com>
2021-02-23 12:54:30 +0800

    build(article-service): springboot project initialize

---
+++ article-service/src/main/java/dev/huhao/example/realworld/articleservice/DemoApplication.java
@@ -0,0 +1,13 @@
+package dev.huhao.example.realworld.articleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ article-service/src/test/java/dev/huhao/example/realworld/articleservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.huhao.example.realworld.articleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
