2da2a3e141f7eda5c241845dc97266f4c67e9791
Hao Hu <huhao@outlook.com>
2021-02-25 22:55:03 +0800

    docs(migration): add comment for UUID and BINARY(16)

--- article-service/domain/src/main/resources/db/migration/V20210224114725__create_article_table.sql
+++ article-service/domain/src/main/resources/db/migration/V20210224114725__create_article_table.sql
@@ -7,6 +7,7 @@ CREATE TABLE `article`
     -- UUID 类型必须保存成为 BINARY(16)，否则 Spring Data JPA 会在 MySql 环境下报错，类似：
     -- java.sql.SQLException: Incorrect string value: '\x9Bra\x9A\xC22...'
     -- 这也是一个 H2 与 MySql 的差异，如果用 H2 作为 Fake DB，则可以使用 CHAR(36) 那么就会有潜在的不一致风险
+    -- 另一方面，UUID 保存为 BINARY(16)，也能获得更好的性能：https://www.singlestore.com/forum/t/best-uuid-data-type/182
     `author_id`   BINARY(16)   NOT NULL,
     `created_at`  TIMESTAMP(6) NOT NULL,
     `updated_at`  TIMESTAMP(6) NOT NULL
